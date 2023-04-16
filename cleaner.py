79a570e17702c9a6fe62d587b12947b9683514ab
Montefusco Antonio Renato <a.montefusco27@studenti.unisa.it>
2023-04-16 20:18:44 +0200

    Aggiunto parallelismo sul datacleaning
    Implementazione di processi paralleli per velocizzare la pulizia del dataset di training. (utilizziamo il 60% dei processi disponibili dalla cpu)

--- cleaner.py
+++ cleaner.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
+from multiprocessing import Pool, cpu_count
+
 #import tensorflow as tf
 #from transformers import BertTokenizer
 #from transformers import TFBertForSequenceClassification
@@ -59,14 +61,23 @@ def cleanDataset(product_df):
     return product_df
 
 """# Load Dataset"""
+if __name__ == '__main__':
+    train_df = pd.read_csv('datasets/products_train.csv')
+    #Utilizziamo l'60% dei processori disponibili
+    num_processes = cpu_count()*60//100
+    chunk_size = len(train_df)//num_processes
 
-train_df = pd.read_csv('datasets/products_train.csv')
+    chunks= pd.read_csv('datasets/products_train.csv', chunksize=chunk_size)
+    #Generiamo la pool dei processi
+    pool= Pool(processes=num_processes)
 
-"""## Data Cleaning"""
+    cleaned_chunks = pool.map(cleanDataset, chunks)
 
-train_df = cleanDataset(train_df)
-train_df.info()
+    cleaned_df= pd.concat(cleaned_chunks)
 
-"""## Saving cleaning dataset to csv"""
+    """## Saving cleaning dataset to csv"""
 
-train_df.to_csv('datasets/products_train_cleaned.csv', index=False)
+    cleaned_df.to_csv('datasets/products_train_cleaned.csv', index=False)
+
+    cleaned_df.info()
+
