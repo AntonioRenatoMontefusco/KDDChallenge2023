cdeb29203174c9b0243cae18668246d5655a47e3
DastLab <datascienceunisa@gmail.com>
2023-05-25 17:33:13 +0200

    fix llama

--- Half_JSON.py
+++ Half_JSON.py
@@ -10,10 +10,10 @@ def split_json_dataset(filename):
     first_half = data[:half_records]
     second_half = data[half_records:]
 
-    with open('Output/first_half.json', 'w') as file:
+    with open('Output/second1_half.json', 'w') as file:
         json.dump(first_half, file, indent=4)
 
-    with open('Output/second_half.json', 'w') as file:
+    with open('Output/second2_half.json', 'w') as file:
         json.dump(second_half, file, indent=4)
 
     print(f"Dataset diviso in due file: 'first_half.json' e 'second_half.json'.")
@@ -31,4 +31,5 @@ def count_json_array_elements(filename):
 
 
 # Esempio di utilizzo
-count_json_array_elements('Output/second_half.json')
+split_json_dataset('Output/second_half.json')
+#%%
