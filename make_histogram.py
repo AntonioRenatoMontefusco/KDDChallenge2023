f363f8ed3b94bf37f7efb08ff34b5258fb165ac0
GalloLuigi <71981111+GalloLuigi@users.noreply.github.com>
2023-06-04 12:35:44 +0200

    Add script for generate histogram

---
+++ make_histogram.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+product_df = pd.read_csv('datasets/products_train.csv')
+sessions_df = pd.read_csv('datasets/sessions_test_task3.csv')
+
+for index, s in sessions_df.iterrows():
+    sessions_df["prev_items"][index] = sessions_df["prev_items"][index].replace("[", "").replace("]", "").replace("'", "")
+    sessions_df["prev_items"][index] = sessions_df["prev_items"][index].split()
+    sessions_df["prev_items"][index] = len(sessions_df["prev_items"][index])
+
+print(sessions_df)
+
+plt.hist(sessions_df["prev_items"])
+plt.xlabel("length")
+plt.ylabel("count")
+plt.title("Length of sessions")
+plt.show()
+
+
+
+
+for index, s in product_df.iterrows():
+    product_df["title"][index] = len(product_df["title"][index].strip())
+print(product_df)
+
+plt.hist(product_df["title"])
+plt.xlabel("length")
+plt.ylabel("count")
+plt.title("Length of titles")
+plt.show()
+
+
+
+for index, s in product_df.iterrows():
+    product_df["brand"][index] = len(product_df["brand"][index].strip())
+
+
+print(product_df)
+
+plt.hist(product_df["brand"])
+plt.xlabel("length")
+plt.ylabel("count")
+plt.title("Length of brands")
+plt.show()
