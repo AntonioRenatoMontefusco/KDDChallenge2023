ccf1a0e1aa6474332e3ef13bdda045352399e9c6
Montefusco Antonio Renato <a.montefusco27@studenti.unisa.it>
2023-06-06 11:42:13 +0200

    fix histogram

--- make_histogram.py
+++ make_histogram.py
@@ -7,24 +7,24 @@ import pandas as pd
 product_df = pd.read_csv('datasets/products_train.csv')
 sessions_df = pd.read_csv('datasets/sessions_test_task3.csv')
 
-for index, s in sessions_df.iterrows():
-    sessions_df["prev_items"][index] = sessions_df["prev_items"][index].replace("[", "").replace("]", "").replace("'", "")
-    sessions_df["prev_items"][index] = sessions_df["prev_items"][index].split()
-    sessions_df["prev_items"][index] = len(sessions_df["prev_items"][index])
-
-print(sessions_df)
-
-plt.hist(sessions_df["prev_items"])
-plt.xlabel("length")
-plt.ylabel("count")
-plt.title("Length of sessions")
-plt.show()
-
+# for index, s in sessions_df.iterrows():
+#     sessions_df["prev_items"][index] = sessions_df["prev_items"][index].replace("[", "").replace("]", "").replace("'", "")
+#     sessions_df["prev_items"][index] = sessions_df["prev_items"][index].split()
+#     sessions_df["prev_items"][index] = len(sessions_df["prev_items"][index])
+#
+# print(sessions_df)
+#
+# plt.hist(sessions_df["prev_items"])
+# plt.xlabel("length")
+# plt.ylabel("count")
+# plt.title("Length of sessions")
+# plt.show()
+#
 
 
 
 for index, s in product_df.iterrows():
-    product_df["title"][index] = len(product_df["title"][index].strip())
+    product_df["title"][index] = len(str(product_df["title"][index]).strip())
 print(product_df)
 
 plt.hist(product_df["title"])
@@ -36,7 +36,7 @@ plt.show()
 
 
 for index, s in product_df.iterrows():
-    product_df["brand"][index] = len(product_df["brand"][index].strip())
+    product_df["brand"][index] = len(str(product_df["brand"][index]).strip())
 
 
 print(product_df)
